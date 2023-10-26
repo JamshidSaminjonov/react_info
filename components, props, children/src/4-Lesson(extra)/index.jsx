8d387bd462800d10681e1cb44bf7e78b071170ca
Jamshid <jsaminjonov5705@gmail.com>
2023-10-26 17:15:13 +0500

    4-darsExtra

--- components, props, children/src/4-Lesson(extra)/index.jsx
+++ components, props, children/src/4-Lesson(extra)/index.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import { ReactDOM } from "react";
 import "./index.css";
-
+// import data from "./data";
+import Students from "./data";
 class Lesson4Extra extends React.Component {
   constructor(e) {
     super(e);
     this.state = {
-      text: "",
-      language: "",
+      // text: "",
+      // language: "",
+      // html: false,
+      // css: false,
+      // js: false,
+      // first: "jamshid",
+      // last: "saminjonov",
     };
   }
   render() {
-    function changing(e) {
-        this.setState({})  
-      switch (e) {
-        case "uz":
-          return "uzbek";
-        case "ru":
-          return "rus";
-        case "en":
-          return "english";
-      }
-    }
+    // function changing(e) {
+    //   switch (e) {
+    //     case "uz":
+    //       return "uzbek";
+    //     case "ru":
+    //       return "rus";
+    //     case "en":
+    //       return "english";
+    //   }
+    // }
+    // let basa = Students;
+
+    // const multiChange = (key, value) => {
+    //   this.setState({ [key]: value });
+    // };
     return (
       <>
         <div className="input-change">
@@ -30,16 +40,87 @@ class Lesson4Extra extends React.Component {
             onChange={(e) => this.setState({ text: e.target.value })}
           />
           <p>{this.state.text}</p> */}
-          <select
+          {/* <select
             name="sel"
             id="sel"
-            onChange={(e) => this.setState({language:changing(e.target.value))}}
+            onChange={(e) => this.setState({ language: e.target.value })}
           >
             <option value="uz">Uz</option>
             <option value="en">EN</option>
             <option value="ru">Ru</option>
           </select>
-          <p> bu til : {this.state.language}</p>
+          <p> bu til : {changing(this.state.language)}</p> */}
+          {/* <label htmlFor="">
+            HTML{" "}
+            <input
+              type="checkbox"
+              onChange={(e) => this.setState({ html: e.target.checked })}
+            />
+          </label>
+          <label htmlFor="">
+            CSS{" "}
+            <input
+              type="checkbox"
+              onChange={(e) => this.setState({ css: e.target.checked })}
+            />
+          </label>
+          <label htmlFor="">
+            JS{" "}
+            <input
+              type="checkbox"
+              onChange={(e) => this.setState({ js: e.target.checked })}
+            />
+          </label>
+          <p>
+            Foydalanuvchi {this.state.html && "html"} {this.state.css && "css"}{" "}
+            {this.state.js && "js"}{" "}
+            {this.state.html || this.state.css || this.state.js
+              ? "ni biladi"
+              : "hech narsani bilmaydi"}
+          </p> */}
+          {/* <table border={1} width={"600px"}>
+            <thead>
+              <tr>
+                <th style={{ position: "sticky", top: 0, left: 0 }}>ID</th>
+                <th>NAME</th>
+                <th>AGE</th>
+                <th>STAUS</th>
+                <th>OCCUPATION</th>
+                <th style={{ position: "sticky", top: 0, right: 0 }}>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {basa.map((v) => {
+                return (
+                  <tr>
+                    <td style={{ position: "sticky", top: 0, left: 0 }}>
+                      {v.id}
+                    </td>
+                    <td>{v.name}</td>
+                    <td>{v.age}</td>
+                    <td>{v.status}</td>
+                    <td>{v.occupation}</td>
+                    <td style={{ position: "sticky", top: 0, right: 0 }}>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table> */}
+          {/* <input
+            type="text"
+            name="first"
+            onChange={(e) => multiChange(e.target.name, e.target.value)}
+          />
+          <input
+            type="text"
+            name="last"
+            onChange={(e) => multiChange(e.target.name, e.target.value)}
+          />
+          <p>
+            Firstname:{this.state.first} Lastname:{this.state.last}
+          </p> */}
         </div>
       </>
     );
